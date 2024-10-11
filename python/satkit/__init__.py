13ee01bc4396990b727386a625769800b043b227
Steven Michael <ssmichael@gmail.com>
2024-10-10 22:08:48 -0400

    suppress python warning

--- python/satkit/__init__.py
+++ python/satkit/__init__.py
@@ -2,7 +2,7 @@ from .satkit import *
 
 from ._version import *
 
-if utils.datafiles_exist() == False:
+if utils.datafiles_exist() == False: # type: ignore
     print(f"Could not find necessary data files.")
     print(f'Run "satkit.utils.update_datafiles()" to download necessary files')
     print(f"This includes JPL ephemeris, gravity, space weather, ")
