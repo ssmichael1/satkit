885f7f5937918be8aa332ce89611206e8c2eca6d
Steven Michael <ssmichael@gmail.com>
2024-10-13 07:44:36 -0400

    forgot to add file

---
+++ src/orbitprop/precomputed.rs
@@ -0,0 +1,69 @@
+use crate::frametransform::qgcrf2itrf_approx;
+use crate::jplephem;
+use crate::types::*;
+use crate::utils::SKResult;
+use crate::AstroTime;
+use crate::Duration;
+use crate::SolarSystem;
+
+pub type InterpType = (Quaternion, Vector3, Vector3);
+
+pub struct Precomputed {
+    pub start: AstroTime,
+    pub stop: AstroTime,
+    pub step: f64,
+    data: Vec<InterpType>,
+}
+
+impl Precomputed {
+    pub fn new(start: &AstroTime, stop: &AstroTime) -> SKResult<Precomputed> {
+        let step: f64 = 60.0;
+
+        Ok(Precomputed {
+            start: start.clone(),
+            stop: stop.clone(),
+            step: {
+                match stop > start {
+                    true => step.abs(),
+                    false => -1.0 * step.abs(),
+                }
+            },
+            data: {
+                let nsteps: usize =
+                    2 + ((stop - start).seconds().abs() / step.abs()).ceil() as usize;
+                println!("nsteps: {}", nsteps);
+                let mut data = Vec::new();
+                data.reserve(nsteps);
+                for idx in 0..nsteps {
+                    let t = *start + Duration::Seconds((idx as f64) * step);
+                    let q = qgcrf2itrf_approx(&t);
+                    let psun = jplephem::geocentric_pos(SolarSystem::Sun, &t)?;
+                    let pmoon = jplephem::geocentric_pos(SolarSystem::Moon, &t)?;
+                    data.push((q, psun, pmoon));
+                }
+                data
+            },
+        })
+    }
+
+    pub fn interp(&self, t: &AstroTime) -> SKResult<InterpType> {
+        if self.step > 0.0 {
+            if *t < self.start || *t > self.stop {
+                return Err("Precomputed::interp: time is outside of precomputed range".into());
+            }
+        } else {
+            if *t > self.start || *t < self.stop {
+                return Err("Precomputed::interp: time is outside of precomputed range".into());
+            }
+        }
+
+        let idx = (t - self.start).seconds() / self.step;
+        let delta = idx - idx.floor();
+        let idx = idx.floor() as usize;
+
+        let q = self.data[idx].0.slerp(&self.data[idx + 1].0, delta);
+        let psun = self.data[idx].1 + (self.data[idx + 1].1 - self.data[idx].1) * delta;
+        let pmoon = self.data[idx].2 + (self.data[idx + 1].2 - self.data[idx].2) * delta;
+        Ok((q, psun, pmoon))
+    }
+}
