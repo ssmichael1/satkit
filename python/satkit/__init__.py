5853b026818eed647edbb17c5d1094be373a3562
Steven Michael <ssmichael@gmail.com>
2025-07-30 10:26:24 -0400

    up version

--- python/satkit/__init__.py
+++ python/satkit/__init__.py
@@ -5,6 +5,7 @@ import sys
 
 if utils.datafiles_exist() == False:  # type: ignore
     print(f"Could not find necessary data files.", file=sys.stderr)
+    print(f"Install satkit-data package via pypi or", file=sys.stderr)
     print(f'Run "satkit.utils.update_datafiles()" to download necessary files', file=sys.stderr)
     print(f"This includes JPL ephemeris, gravity, space weather, ", file=sys.stderr)
     print(f"Earth orientation parameters, leap seconds, and coefficients", file=sys.stderr)
