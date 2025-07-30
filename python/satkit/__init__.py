f28b39ba3fb6b1a2ccef3df34f2748de6991101f
Steven Michael <ssmichael@gmail.com>
2025-07-30 10:23:06 -0400

    log python warnings about missing data files to stderr instead of stdout

--- python/satkit/__init__.py
+++ python/satkit/__init__.py
@@ -1,10 +1,11 @@
-from .satkit import *
+from .satkit import * # type: ignore
 
 from ._version import *
+import sys
 
 if utils.datafiles_exist() == False:  # type: ignore
-    print(f"Could not find necessary data files.")
-    print(f'Run "satkit.utils.update_datafiles()" to download necessary files')
-    print(f"This includes JPL ephemeris, gravity, space weather, ")
-    print(f"Earth orientation parameters, leap seconds, and coefficients")
-    print(f"for inertial-to-Earth-fixed transforms")
+    print(f"Could not find necessary data files.", file=sys.stderr)
+    print(f'Run "satkit.utils.update_datafiles()" to download necessary files', file=sys.stderr)
+    print(f"This includes JPL ephemeris, gravity, space weather, ", file=sys.stderr)
+    print(f"Earth orientation parameters, leap seconds, and coefficients", file=sys.stderr)
+    print(f"for inertial-to-Earth-fixed transforms", file=sys.stderr)
