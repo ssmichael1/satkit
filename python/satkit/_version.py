909423ea3aa67c437d81c0ef0764bf69790af26e
Steven Michael <ssmichael@gmail.com>
2025-08-24 14:13:27 -0400

    fix python _version.py to export "version"

--- python/satkit/_version.py
+++ python/satkit/_version.py
@@ -1,2 +1,18 @@
-# This file is auto-generated. Update the version here to match pyproject.toml.
-__version__ = "0.8.0"
+__all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
+
+TYPE_CHECKING = False
+if TYPE_CHECKING:
+    from typing import Tuple
+    from typing import Union
+
+    VERSION_TUPLE = Tuple[Union[int, str], ...]
+else:
+    VERSION_TUPLE = object
+
+version: str
+__version__: str
+__version_tuple__: VERSION_TUPLE
+version_tuple: VERSION_TUPLE
+
+__version__ = version = "0.8.1"
+__version_tuple__ = version_tuple = (0, 8, 1)
