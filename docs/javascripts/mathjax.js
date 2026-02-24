fc36569f95d78b23ffa88c5bcb90384fdafd71f4
Steven Michael <ssmichael@gmail.com>
2026-02-24 10:34:01 -0500

    make sure equations load before rendering

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -5,14 +5,23 @@ window.MathJax = {
     processEscapes: true,
     processEnvironments: true,
   },
+  startup: {
+    // Don't auto-typeset on load; we control it via document$
+    typeset: false,
+  },
 };
 
-// Re-typeset MathJax after MkDocs Material instant navigation
+// Re-typeset MathJax after MkDocs Material instant navigation.
+// On first load MathJax may not be ready yet, so wait for its promise.
 document$.subscribe(function () {
-  if (typeof MathJax !== "undefined" && MathJax.typesetPromise) {
+  var typeset = function () {
     MathJax.startup.output.clearCache();
     MathJax.typesetClear();
     MathJax.texReset();
     MathJax.typesetPromise();
+  };
+
+  if (typeof MathJax !== "undefined" && MathJax.startup && MathJax.startup.promise) {
+    MathJax.startup.promise.then(typeset);
   }
 });
